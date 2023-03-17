59acecdbe759d34370a2c97f95f2a1080b47666c
vsenikiperebral <vsenikiperebral@gmail.com>
2023-03-17 18:47:52 +0200

    17.03 / 18.47

--- JS/map.js
+++ JS/map.js
@@ -1,30 +1,28 @@
 // Get the modal
-const modal = document.getElementById("myModal");
+const modal = document.getElementById('myModal');
 
 // Get the button that opens the modal
-const btn = document.getElementById("myBtn");
+const btn = document.getElementById('myBtn');
 
 // Get the <span> element that closes the modal
-const span = document.getElementsByClassName("close")[0];
+const span = document.getElementsByClassName('close')[0];
 
 // When the user clicks the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+btn.onclick = function () {
+  modal.style.display = 'block';
+};
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+span.onclick = function () {
+  modal.style.display = 'none';
+};
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
-    modal.style.display = "none";
+    modal.style.display = 'none';
   }
-}
-
-
+};
 
 // gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
@@ -37,19 +35,16 @@ window.onclick = function(event) {
 //     })
 //  }
 
+window.addEventListener('scroll', function () {
+  const header = document.querySelector('#main-header');
+  const nav = document.querySelector('#main-nav');
+  const scrollPosition = window.pageYOffset;
 
-window.addEventListener('scroll', function() {
-    var header = document.querySelector('#main-header');
-    var nav = document.querySelector('#main-nav');
-    var scrollPosition = window.pageYOffset;
-  
-    if (scrollPosition > 50) {
-      header.classList.add('shrink');
-      nav.classList.add('shrink');
-    } else {
-      header.classList.remove('shrink');
-      nav.classList.remove('shrink');
-    }
-  });
-  
-  
+  if (scrollPosition > 50) {
+    header.classList.add('shrink');
+    nav.classList.add('shrink');
+  } else {
+    header.classList.remove('shrink');
+    nav.classList.remove('shrink');
+  }
+});
